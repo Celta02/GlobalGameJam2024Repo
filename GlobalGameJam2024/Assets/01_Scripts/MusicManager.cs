6501e5d890a67ad7a6268acf0dfad162f8ebe677
Celta02 <r.espinoza.tagle@gmail.com>
2024-01-28 14:59:20 -0300

    MUSIC IMPLEMENTATION

---
+++ GlobalGameJam2024/Assets/01_Scripts/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+
+namespace CeltaGames
+{
+    public class MusicManager : MonoBehaviour
+    {
+        [Header("Music")]
+        [SerializeField] EventReference _mainTheme;
+        [SerializeField] EventReference _goodEnding;
+        [SerializeField] EventReference _badEnding;
+        
+        EventInstance _mainThemeInstance;
+        EventInstance _goodEndingInstance;
+        EventInstance _badEndingInstance;
+
+        void Awake()
+        {
+            _mainThemeInstance = RuntimeManager.CreateInstance(_mainTheme);
+            _goodEndingInstance = RuntimeManager.CreateInstance(_goodEnding);
+            _badEndingInstance = RuntimeManager.CreateInstance(_badEnding);
+        }
+
+        void Start()
+        {
+            _mainThemeInstance.start();
+        }
+    }
+}
