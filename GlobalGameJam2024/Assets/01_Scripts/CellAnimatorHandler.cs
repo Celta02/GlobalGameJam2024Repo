ce5b35dddfcefb8553dd5a9017dcbe18289e8a2a
Celta02 <r.espinoza.tagle@gmail.com>
2024-01-29 17:17:05 -0300

    PLAY AND REPLAY BUTTONS

---
+++ GlobalGameJam2024/Assets/01_Scripts/CellAnimatorHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using CeltaGames.ScriptableObjects;
+using UnityEngine;
+
+namespace CeltaGames
+{
+    public class CellAnimatorHandler : MonoBehaviour
+    {
+        [SerializeField] PlayerPosition _position;
+        [SerializeField] PlayerGrid _grid;
+        [SerializeField] PlayerHitEmitter _hitEmitter;
+        
+        [Header("Animation Sprites")]
+        [SerializeField] Sprite _orangeAnim;
+        [SerializeField] Sprite _blueAnim;
+        [SerializeField] Sprite _greenAnim;
+        [SerializeField] Sprite _yellowAnim;
+        [SerializeField] Sprite _CyanAnim;
+
+        PlayerHitListener _hitListener;
+
+        void OnEnable() => _hitEmitter.RegisterListener(_hitListener);
+        void OnDisable() => _hitEmitter.UnregisterListener(_hitListener);
+
+        void Awake() => _hitListener = new PlayerHitListener(OnHit);
+
+        void OnHit(bool wasHitSuccessful, HitBoxType type)
+        {
+            
+        }
+
+        IEnumerator AnimateMouse()
+        {
+            
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
---
+++ GlobalGameJam2024/Assets/01_Scripts/PlayButton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace CeltaGames
+{
+    public class PlayButton : MonoBehaviour
+    {
+        SceneLoader _sceneLoader = new SceneLoader();
+
+        public void OnPlayButtonClicked() => _sceneLoader.LoadMainScene();
+    }
+}
